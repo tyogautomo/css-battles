4fd748683b6fe6cf4ade18221f34afafa0b9028f
tyogautomo <yoga.utomo27@gmail.com>
2021-05-01 21:43:33 +0700

    corona done

--- src/pages/CoronaVirus/CoronaVirus.page.jsx
+++ src/pages/CoronaVirus/CoronaVirus.page.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 import styles from './CoronaVirus.module.css';
 
 const CoronaVirus = () => {
+  const dots = [{ name: 'dot_big' }, { name: 'dot_medium' }, { name: 'dot_small' }];
+  const spikes = [{ name: 'spk_vertical' }, { name: 'spk_diagonal1' }, { name: 'spk_diagonal2' }]
   return (
     <div className={styles.container}>
-      <div className={styles.center}>
-        <div className={`${styles.dot} ${styles.dot_80px}`}></div>
-        <div className={`${styles.dot} ${styles.dot_40px}`}></div>
-        <div className={`${styles.dot} ${styles.dot_20px}`}></div>
+      <div className={styles.virus_body}>
+        {dots.map(dot => <div className={`${styles.dot} ${styles[dot.name]}`}></div>)}
+        {spikes.map(spike => <div className={`${styles.spike} ${styles[spike.name]}`}></div>)}
       </div>
     </div>
   );
